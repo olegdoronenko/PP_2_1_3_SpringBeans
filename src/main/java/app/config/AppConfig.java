acfa80d7f78c754f21239785f7f9257c65130e89
olegdoronenko <61374593+olegdoronenko@users.noreply.github.com>
2023-04-16 10:43:20 +0300

    работает но странно Тест проходит, но через костыль - сделал в AnimalsCage метод getTimer нужна помощь

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,34 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+//    @Bean(name = "dog")
+//    public Animal getDog() {
+//        Animal animal;
+//        animal = new Dog();
+//        return animal;
+//    }
+//
+//    @Bean(name = "cat")
+//    public Animal getCat() {
+//        Animal animal;
+//        animal = new Cat();
+//        return animal;
+//    }
+//
+//    @Bean(name = "timer")
+//    public Timer getTimer() {
+//        Timer timer;
+//        timer = new Timer();
+//        return timer;
+//    }
 }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,36 @@
 package app.model;
 
+import jdk.jfr.Name;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AnimalsCage {
 
     @Autowired
-    private Animal animal;
+    @Qualifier( "dog")
+    private Animal animal1;
+
+    @Autowired
+    @Qualifier("cat")
+    private Animal animal2;
+
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animal1.toString() +", " + animal2.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
 }
